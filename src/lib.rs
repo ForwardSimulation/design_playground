e2230f8f587a0e92a0ad5d086cad4925141a6569
Kevin R. Thornton <krthornt@uci.edu>
2023-04-06 12:32:07 -0700

    quickly get some ideas on paper

---
+++ src/lib.rs
@@ -0,0 +1,104 @@
+use forrustts::genetics::GeneticMap;
+use forrustts::prelude::*;
+
+// We need a type with a more complex
+// layout than a simple position.
+// It is > 32 bits just to try
+// to annoy memory accesses.
+pub struct Mutation {
+    position: Position,
+    effect_sizes: Vec<f64>,
+    scratch: u32,
+}
+
+impl Mutation {
+    pub fn new(position: Position, effect_sizes: Vec<f64>) -> Self {
+        Self {
+            position,
+            effect_sizes,
+            scratch: 0,
+        }
+    }
+
+    pub fn position(&self) -> Position {
+        self.position
+    }
+
+    pub fn effect_sizes(&self) -> &[f64] {
+        &self.effect_sizes
+    }
+}
+
+struct MutationRange {
+    start: usize,
+    stop: usize,
+}
+
+#[derive(Default)]
+struct Haplotypes {
+    haplotypes: Vec<MutationRange>,
+    mutations: Vec<usize>,
+}
+
+#[derive(Copy, Clone, Debug)]
+struct DiploidGenome {
+    first: usize,
+    second: usize,
+}
+
+impl DiploidGenome {
+    pub fn new(first: usize, second: usize) -> Self {
+        Self { first, second }
+    }
+}
+
+/// When will the borrow checker hate this?
+pub struct DiploidPopWithHaplotypes {
+    haplotypes: Haplotypes,
+    individuals: Vec<Option<DiploidGenome>>,
+    mutations: Vec<Mutation>,
+    mutation_counts: Vec<u32>,
+}
+
+impl DiploidPopWithHaplotypes {
+    pub fn new(size: u32) -> Option<Self> {
+        if size > 0 {
+            let individuals = vec![None; size as usize];
+
+            Some(Self {
+                haplotypes: Haplotypes::default(),
+                individuals,
+                mutations: vec![],
+                mutation_counts: vec![],
+            })
+        } else {
+            None
+        }
+    }
+}
+
+pub struct SimParams {
+    pub seed: u64,
+    pub size: u32,
+    pub num_generations: u32,
+    pub mutation_rate: f64,
+}
+
+impl SimParams {
+    pub fn validate(self) -> Option<Self> {
+        if !self.mutation_rate.is_finite() || self.mutation_rate < 0.0 {
+            return None;
+        }
+        Some(self)
+    }
+}
+
+pub fn evolve_pop_with_haplotypes(
+    params: SimParams,
+    genetic_map: GeneticMap,
+) -> Option<DiploidPopWithHaplotypes> {
+    let params = params.validate()?;
+    let mut pop = DiploidPopWithHaplotypes::new(params.size)?;
+
+    Some(pop)
+}

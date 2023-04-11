0a0747da9f14d5f602808ee13abef979a265ed75
Kevin R. Thornton <krthornt@uci.edu>
2023-04-11 15:27:52 -0700

    works, but much slower than we'd like

--- src/fwdpp_copy.rs
+++ src/fwdpp_copy.rs
@@ -96,17 +96,6 @@ fn get_parental_genome(genomes: &[HaploidGenome], genome: usize) -> ParentalGeno
     }
 }
 
-fn get_parental_genomes<'a>(
-    parents: &'a [DiploidGenome],
-    genomes: &'a [HaploidGenome],
-    parent: usize,
-) -> (ParentalGenome<'a>, ParentalGenome<'a>) {
-    (
-        get_parental_genome(genomes, parents[parent].first),
-        get_parental_genome(genomes, parents[parent].second),
-    )
-}
-
 fn get_mendelized_parent_genome_indexes(
     individuals: &[DiploidGenome],
     parent: usize,
@@ -129,7 +118,7 @@ fn update_genomes(
     genome_queue: &mut Vec<usize>,
 ) -> usize {
     let (genome1, genome2) = genomes;
-    let (first_genome_index, update) =
+    let (output_genome_index, update) =
         if mutations.is_empty() && genetic_map.breakpoints().is_empty() {
             pop.haplotypes[genome1].count += 1;
             (genome1, None)
@@ -137,9 +126,14 @@ fn update_genomes(
             match genome_queue.pop() {
                 Some(index) => {
                     pop.haplotypes[index].mutations.clear();
-                    pop.haplotypes[index].count += 1;
-                    unimplemented!("we need to say to populate this genome.");
-                    (index, None)
+                    assert_eq!(pop.haplotypes[index].count, 0);
+                    (
+                        index,
+                        Some(HaploidGenome {
+                            mutations: vec![],
+                            count: 0,
+                        }),
+                    )
                 }
                 None => {
                     let rv = pop.haplotypes.len();
@@ -158,7 +152,6 @@ fn update_genomes(
             get_parental_genome(&pop.haplotypes, genome1),
             get_parental_genome(&pop.haplotypes, genome2),
         );
-        println!("{mutations:?}, {:?}, {:?}", genomes.0, genomes.1);
         generate_offspring_genome(
             genomes,
             &pop.mutations,
@@ -169,14 +162,14 @@ fn update_genomes(
             &mut genome.mutations,
         );
         genome.count += 1;
-        assert_eq!(first_genome_index, pop.haplotypes.len());
-        println!("after = {first_genome_index}, {:?}, {:?},", genome, genomes,);
-        pop.haplotypes.push(genome);
-        assert_eq!(first_genome_index, pop.haplotypes.len() - 1);
+        if output_genome_index < pop.haplotypes.len() {
+            std::mem::swap(&mut pop.haplotypes[output_genome_index], &mut genome);
+        } else {
+            pop.haplotypes.push(genome);
+        }
     }
 
-    println!("returning {first_genome_index}");
-    first_genome_index
+    output_genome_index
 }
 
 #[inline(never)]
@@ -200,8 +193,8 @@ pub fn evolve_pop_with_haplotypes(
     let mut genetic_map = genetic_map;
 
     //let mut parent_haplotype_map = vec![];
+    let mut offspring: Vec<DiploidGenome> = vec![];
     for generation in 0..params.num_generations {
-        let mut offspring: Vec<DiploidGenome> = vec![];
         let mut genome_queue = make_haploid_genome_queue(&pop.haplotypes);
         for g in &mut pop.haplotypes {
             g.count = 0;
@@ -232,20 +225,19 @@ pub fn evolve_pop_with_haplotypes(
             let nm = mutations.len();
             let first = update_genomes(
                 (genome1, genome2),
-                mutations.clone(),
+                mutations,
                 &genetic_map,
                 &mut pop,
                 2 * params.num_individuals,
                 &mut genome_queue,
             );
-            println!("got {first}");
             assert!(pop.haplotypes[first].count > 0);
             if nm > 0 {
                 assert!(
                     !pop.haplotypes[first].mutations.is_empty(),
                     "{first} {:?}, {:?}",
                     pop.haplotypes[first],
-                    mutations,
+                    nm,
                 );
             }
 
@@ -277,18 +269,11 @@ pub fn evolve_pop_with_haplotypes(
             offspring.push(DiploidGenome { first, second });
         }
         std::mem::swap(&mut pop.individuals, &mut offspring);
-        for i in &pop.individuals {
-            assert!(pop.haplotypes[i.first].count > 0);
-            assert!(pop.haplotypes[i.second].count > 0);
-        }
+        //for i in &pop.individuals {
+        //    assert!(pop.haplotypes[i.first].count > 0);
+        //    assert!(pop.haplotypes[i.second].count > 0);
+        //}
         pop.count_mutations();
-        //println!(
-        //    "{} {} | {} {}",
-        //    pop.haplotypes.len(),
-        //    pop.haplotypes.capacity(),
-        //    pop.mutations.len(),
-        //    pop.mutations.capacity()
-        //);
         offspring.clear();
     }
     Some(pop)
